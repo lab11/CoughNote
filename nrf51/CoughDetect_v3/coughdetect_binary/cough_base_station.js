78e3ea00689523a355b96e9484dff7e50169514d
Will Huang <wwhuang@umich.edu>
2017-03-31 02:00:15 -0400

    changes to ble time sync, base statoin side. time is super off

--- nrf51/CoughDetect_v3/coughdetect_binary/cough_base_station.js
+++ nrf51/CoughDetect_v3/coughdetect_binary/cough_base_station.js
@@ -1,145 +1,76 @@
 var noble = require('noble')
-var csvWriter = require('csv-write-stream')
 var moment = require('moment')
-var fs = require('fs')
 
-var target_mac = ''
+var target_id = 1
 
-var cough_service_uuid = 'c52c02194c8a1f89b9402b308635169c'
-var cough_data_uuid = 	 'c52c02014c8a1f89b9402b308635169c' // sensor sends over cough times in this
-var cough_ack_uuid =  	 'c52c02024c8a1f89b9402b308635169c' // we use this to tell sensor we're ready 
-var cough_end_uuid =  	 'c52c02034c8a1f89b9402b308635169c' // sensor tells us it's time to disconnect
-var cough_id_uuid  = 	 'c52c02044c8a1f89b9402b308635169c' // sensor tells us it's id
-var cough_time_uuid =  	 'c52c02054c8a1f89b9402b308635169c' // cough time
+var cough_service_uuid = 	'c52c02194c8a1f89b9402b308635169c'
+var base_station_rdy_uuid = 'c52c02014c8a1f89b9402b308635169c' // sensor sends over cough times in this
+var sys_time_uuid =  	 	'c52c02024c8a1f89b9402b308635169c' // we use this to tell sensor we're ready 
 
 var cough_peripheral = null
 var cough_service = null
-var cough_data_char = null
-var cough_ack_char = null
-var cough_end_char = null
-var cough_id_char = null
-var cough_time_char = null
-var current_cough_id = null
+var base_station_rdy_char = null 
+var sys_time_char = null
 var restart_counter = 0
-var writer = csvWriter({headers: ['UTC TIME']})
 
-var ackBuffer = new Buffer([1])
+var base_station_rdy_buf = Buffer.alloc(1, 1)
+var sys_time_buf = Buffer.alloc(4)
+
 // Peripheral Callbacks 
-
-function pWriteHandle(error) {
-	console.log("Write handle called")
+function baseWriteCallback(error) {
+	console.log("baseWrite callback")
 }
 
-function ackWriteCallback(error) {
-	console.log("Ack write callback")
+function sysTimeWriteCallback(error) {
+	console.log("sysTimeWrite cllback")
 }
 
-function writeAck() {
-	cough_ack_char.write(ackBuffer, false, ackWriteCallback)
+function baseRdyCallback(data, isNoitification) {
+	console.log("BASE WRITE WTF")
 }
 
-function dataCharCallback(data, isNoitification) {
-	var cough_time = data.readUIntLE(0,4)
-	console.log("Data char callback: " + cough_time)
-	writer.write([cough_time])
-	writeAck()
-}
-
-function timeCharCallback(data, isNoitification) {
-	var ntime = data.readUIntLE(0, 4)
-	console.log("Time char callback: " + ntime)
+function sysTimeCallback(data, isNoitification) {
 	var ctime = moment().unix() 
+	var ntime = data.readUInt32LE(0)
 	var tdiff = ctime - ntime
-	writer.write([tdiff])
-	cough_ack_char.write(ackBuffer, false, ackWriteCallback)
+	console.log("Time char callback: " + ntime)
+	console.log([target_id, ctime, ntime, tdiff])
 }
 
-function idCharCallback(data, isNoitification) {
-	console.log("Got ID of length: " + data.length)
-	console.log("ID: ", + data[0])
-	var m_id = data[0]
-	fname = "data/" + String(m_id) + "_" + moment().format() + ".csv"
-	writer.pipe(fs.createWriteStream(fname))
-	cough_ack_char.write(ackBuffer, false, ackWriteCallback)
-}
-
-function endCharCallback(data, isNoitification) {
-	console.log("End char received")
-	cough_peripheral.disconnect(function(error) {
-		console.log("disconnect handler")
-		if(error) throw error
-	})
-}
-
-function getTimeChar() {
-	cough_service.discoverCharacteristics([cough_time_uuid], function(error, characteristics) {
-		console.log("got time char")
-		if(error) throw error
-	
-		cough_time_char	 = characteristics[0]
-		cough_time_char.on('data', timeCharCallback) 
-		cough_time_char.notify(true, function(error) {
-			console.log("time notify set. Char setting done")
-			if(error) {throw error}
-			cough_ack_char.onc	
-			cough_ack_char.write(ackBuffer, false, ackWriteCallback)
-		})
-	})
-}
-
-function getIdChar() {
-	cough_service.discoverCharacteristics([cough_id_uuid], function(error, characteristics) {
-		console.log("got id char")
+function getSysTimeChar() {
+	cough_service.discoverCharacteristics([sys_time_uuid], function(error, characteristics) {
+		console.log("Got SysTime char")
 		if(error) {throw error}
-		cough_id_char = characteristics[0]
-		cough_id_char.on('data', idCharCallback)
-		cough_id_char.notify(true, function(error) {
-			console.log('id notify set')
+		if(characteristics.length != 1) {
+			console.log("data discovery fucked - SysTime")
+			process.exit()
+		}
+		sys_time_char = characteristics[0]
+		sys_time_char.on('data', sysTimeCallback)
+		sys_time_char.notify(true, function(error) {
+			console.log("SysTime notify set")
 			if(error) {throw error}
-			getTimeChar()
+			var ctime = moment().unix() 
+			sys_time_buf.writeUInt32LE(ctime, 0)
+			sys_time_char.write(sys_time_buf, false, function(error) {
+				console.log("Time Written")
+			})
 		})
-	})
-}
-
-function getEndChar() {
-	cough_service.discoverCharacteristics([cough_end_uuid], function(error, characteristics) {
-		console.log("got end char")
-		if(error) {throw error} 
-		cough_end_char = characteristics[0]
-		cough_end_char.on('data', endCharCallback)
-		cough_end_char.notify(true, function(error) {
-			console.log("End char notify set")
-			if(error) {throw error}
-			getIdChar()
-		})
-	})
-}
-
-function getAckChar() {
-	cough_service.discoverCharacteristics([cough_ack_uuid], function(error, characteristics) {
-		console.log("Got ack char")
-		if(error) {throw error}
-		cough_ack_char = characteristics[0]
-		getEndChar()
 	})
 }
 
 function pGetCharacteristics() {
-	// Gets all the characteristics and sets all their shit.
-	cough_service.discoverCharacteristics([cough_data_uuid], function(error, characteristics) {
-		console.log("Got data char")
+	// Start a chain of getting characteristics. The first part of the chain grabs the base station ready char
+	cough_service.discoverCharacteristics([base_station_rdy_uuid], function(error, characteristics) {
+		console.log("Got base station ready  char")
 		if(error) {throw error}
 		if(characteristics.length != 1) {
-			console.log("data discovery fucked")
+			console.log("data discovery fucked - BaseStationRdy")
+			console.log(characteristics.length)
 			process.exit()
 		}
-		cough_data_char = characteristics[0]
-		cough_data_char.on('data', dataCharCallback)
-		cough_data_char.notify(true, function(error) {
-			console.log("data notify set")
-			if(error) {throw error}
-			getAckChar()
-		})
+		base_station_rdy_char = characteristics[0]
+		getSysTimeChar()
 	})
 }
 
@@ -188,6 +119,23 @@ function discoverCallback(peripheral) {
 	if(peripheral['advertisement']['localName'] == 'CoughDet') {
 		console.log("Found a cough detector!")
 		console.log(peripheral)
+		var mfgData = peripheral['advertisement']['manufacturerData']
+		var periphId = mfgData.readUInt8(2)
+		if(periphId == target_id) {
+			console.log('Found target CoughDetector')
+			noble.stopScanning(function() {
+			console.log("Stopped scanning, time to connect")
+			cough_peripheral = peripheral
+				cough_peripheral.once('disconnect', function() {
+					console.log("Disconnect once handler")
+				})
+				cough_peripheral.connect(function(error) {
+					if(error) {throw error}
+					console.log("iniital connection")
+					setTimeout(pConnectHandler, 100);
+				})	
+			})
+		}
 	}
 }
 
